4cc7a7e2bae8ef52cd78cb640b8e562c61700a10
Camo <vladimir.camaj@gmail.com>
2015-01-10 15:21:35 +0100

    initial commit

---
+++ app/presenters/DefaultPresenter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
+
+class DefaultPresenter extends BasePresenter
+{
+	public function startup()
+	{
+		parent::startup();
+
+	}
+
+
+	public function renderDefault()
+	{
+		$this->template->anyVariable = 'any value';
+	}
+
+}
